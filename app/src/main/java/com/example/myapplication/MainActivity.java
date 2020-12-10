9623bce8deed25732b46c34532f238f0facca0af
jiachuangliu <490436534@qq.com>
2020-12-10 22:58:44 +0800

    test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("9999","ssdd.");
+
         setContentView(R.layout.activity_main);
     }
 }
